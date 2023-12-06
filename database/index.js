7ea2d84fac3365eca281c0e8df78ad8b2286b144
Erick Owen Indri Kalumata <erickkalumata106@student.unsrat.ac.id>
2023-12-06 21:26:19 +0800

    add db config

--- database/index.js
+++ database/index.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port:process.env.DB_PORT,
 });
 
 module.exports = pool.promise();
--- routes/post.router.js
+++ routes/post.router.js
@@ -4,4 +4,9 @@ const router = express.Router();
 const postController = require('../controller/post.controller');
 
 router.get('/', postController.findAll);
+router.get('/:id', postController.getById);
+router.post('/', postController.create);
+router.put('/:id', postController.update);
+router.delete('/:id', postController.delete);
+
 module.exports = router;
