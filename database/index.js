6755979d334cc30096d6f90526c9c259c13efd0a
Munawarotul <afidohpml@gmail.com>
2023-12-11 10:29:11 +0700

    nambah port

--- database/index.js
+++ database/index.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool.promise();
