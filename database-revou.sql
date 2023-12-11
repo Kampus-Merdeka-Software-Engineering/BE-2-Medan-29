7fef5ccec2d39b8abdbe92694a5ad065e0d96738
Munawarotul <afidohpml@gmail.com>
2023-12-11 10:58:27 +0700

    database auto increment

--- database-revou.sql
+++ database-revou.sql
@@ -107,7 +107,7 @@ INSERT INTO `kategori` (`id`, `Kategori`) VALUES
 
 CREATE TABLE `user_berita` (
   `email_user` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `id` int NOT NULL
+  `id` int NOT NULL AUTO_INCREMENT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
